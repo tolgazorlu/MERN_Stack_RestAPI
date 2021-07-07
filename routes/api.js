77a04d6c3ac17df0b793847c791fd08f151209da
tolgazorlu <tolgazorlu17@gmail.com>
2021-07-07 19:44:38 +0300

    create restapi-project and publish

---
+++ routes/api.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+
+//get a list of ninjas from the db
+router.get('/ninjas', function(req, res){
+    res.send({type:'GET'});
+});
+
+
+//add a new ninja to the db
+router.post('/ninjas', function(req, res){
+    console.log(req.body);
+    res.send({
+        type:'POST',
+        name: req.body.name,
+        rank: req.body.rank
+    });
+});
+
+
+//update a ninja in the db
+router.put('/ninjas/:id', function(req, res){
+    res.send({type:'PUT'});
+});
+
+//delete a ninja from db
+router.delete('/ninjas/:id', function
